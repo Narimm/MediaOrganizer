e278c54a301b652c4164acd0cac7080340f9343d
benjamincharlton <benjicharlton@gmail.com>
2013-12-04 14:07:47 +1000

    create package add docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","FileReorder()"],["f","GetExifOriginalTime()"],["f","GetFileCreateTime()"],["f","GetFiles()"],["f","GetFilesRecursive()"],["f","GetTimefromFilename()"],["f","NmCDFile()"]];
